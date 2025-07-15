c115cb165ad857786ac6c814633414d4f888a1b0
rune-philip <rune@bjoed.com>
2025-07-15 20:58:02 +0200

    Fix next.config.js for static export

--- next.config.js
+++ next.config.js
@@ -1,1 +1,9 @@
-module.exports = {};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/LiveTerm',
+  assetPrefix: '/LiveTerm/',
+  trailingSlash: true
+};
+
+module.exports = nextConfig;
