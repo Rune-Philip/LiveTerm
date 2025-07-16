6748b6853e01b03df5ba7a75d95d3a1b27ee253f
Rune-Philip <rpbjoedstrup@gmail.com>
2025-07-16 09:31:43 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: '/LiveTerm',
-  assetPrefix: '/LiveTerm/',
 };
 
 module.exports = nextConfig;
